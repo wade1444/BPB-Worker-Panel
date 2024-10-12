db280118256fa33b9dbe428a4de6d8bb25d91cf7
wade1444 <40153486+wade1444@users.noreply.github.com>
2024-10-13 07:20:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-// <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
+// <!--GAMFC-->version base on commit 1nnd05dcfdr3b723c53c226f81812ffbd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
 // @ts-ignore
 // https://github.com/bia-pain-bache/BPB-Worker-Panel
 
